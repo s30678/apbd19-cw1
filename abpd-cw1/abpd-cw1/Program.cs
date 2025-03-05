78a79a7af9b2f42045f816139df40e71f19d96b3
Maciej Niedzielski <s30678@pjwstk.edu.pl>
2025-03-05 15:17:09 +0100

    kolejny commit

--- abpd-cw1/abpd-cw1/Program.cs
+++ abpd-cw1/abpd-cw1/Program.cs
@@ -1,12 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, nie mi nie działa!");
-Console.WriteLine("jednak cos działa");
-Console.WriteLine("jednak cos działa");
-Console.WriteLine("jednak cos działa");
-Console.WriteLine("jednak cos działa");
-Console.WriteLine("jednak cos działa");
-Console.WriteLine("jednak cos działa");
-Console.WriteLine("jednak cos działa");
-Console.WriteLine("jednak cos działa");
+
+
+
+
+Console.WriteLine("juz dziala i ma sie dobrze" +
+    ":)");
+
 
 
