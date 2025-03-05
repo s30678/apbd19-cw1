c5d1e8a164d4a412ace14ce5782abc3b96ed43ac
Maciej Niedzielski <s30678@pjwstk.edu.pl>
2025-03-05 15:12:14 +0100

    zmiana

--- abpd-cw1/abpd-cw1/Program.cs
+++ abpd-cw1/abpd-cw1/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, zmiana1!");
-Console.WriteLine("Hello, zmiana2, zmieniona zmiana 2!!!!");
-Console.WriteLine("Hello, zmiana3!");
+Console.WriteLine("Hello, nie mi nie działa!");
+
+
